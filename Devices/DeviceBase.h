db9ef09824cf727baa4c2e57c2f98cb6c2f8900b
bddwyx <bddwyx@sjtu.edu.cn>
2025-01-08 21:39:04 +0800

    fix(Devices): DeviceBase析构函数设置为虚函数

--- Devices/DeviceBase.h
+++ Devices/DeviceBase.h
@@ -16,7 +16,8 @@ public:
     static void DevicesHandle();
 
     DeviceBase();
-    ~DeviceBase();
+    virtual ~DeviceBase();
+
     /**
      * 惰性初始化，避免静态变量初始化顺序问题
      * @return
@@ -25,15 +26,16 @@ public:
         static std::list<DeviceBase*> deviceList;
         return deviceList;
     }
+    
     /**
      * 设置分频系数，使设备的执行频率动态可调
      * @param divisionFactor
      */
     void SetDivisionFactor(uint32_t divisionFactor);
-private:
-    uint32_t cnt{0};
-    uint32_t divisionFactor{1};
 
+private:
+    uint32_t cnt = 0;
+    uint32_t divisionFactor = 1;
 };
 
 
