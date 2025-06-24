5fd43c7d3f48c8d57592fef4f745bd5302c4780a
bddwyx <bddwyx@sjtu.edu.cn>
2025-06-24 15:41:31 +0800

    feat(Algorithms): 大版本更新：Algorithm部分
    增加DoubleBuffer功能，更改CRC的实现方式

--- Algorithms/Verification/CRC.cpp
+++ Algorithms/Verification/CRC.cpp
@@ -7,50 +7,30 @@
 #include "CRC.h"
 
 uint8_t CRC8Calc(uint8_t *data, uint16_t length) {
-    const uint8_t polynomial = 0x07; // 标准CRC8多项式
-    uint8_t crc = 0x00; //初始值
+    static etl::crc8_ccitt crc8;
 
-    while (length--) {
-        crc ^= *data++;
-        for (uint8_t i = 0; i < 8; i++) {
-            if (crc & 0x80) {
-                crc = (uint8_t)((crc << 1) ^ polynomial);
-            } else {
-                crc <<= 1; //
-            }
-        }
+    crc8.reset();
+    for (uint16_t i = 0; i < length; i++) {
+        crc8.add(data[i]);
     }
-    return crc;
+    return crc8.value();
 }
 
-uint16_t CRC16Calc(uint8_t *data, uint16_t length){
-    uint16_t crc = 0xffff;        // Initial value
-    while (length--) {
-        crc ^= *data++;            // crc ^= *data; data++;
-        for (uint8_t i = 0; i < 8; ++i) {
-            if (crc & 1)
-                crc = (crc >> 1) ^ 0xA001;        // 0xA001 = reverse 0x8005
-            else
-                crc = (crc >> 1);
-        }
+uint16_t CRC16Calc(uint8_t *data, uint16_t length) {
+    etl::crc16_modbus crc16;
+
+    for (uint16_t i = 0; i < length; i++) {
+        crc16.add(data[i]);
     }
-    return crc;
+    return crc16.value();
 }
 
-uint32_t CRC32Calc(uint8_t *data,uint16_t length) {
-    const uint32_t polynomial = 0xEDB88320; // 标准CRC32多项式
-    uint32_t crc = 0xFFFFFFFF; // 初始值
+uint32_t CRC32Calc(uint8_t *data, uint16_t length) {
+    static etl::crc32 crc32;
 
-    while(length--) {
-        crc ^= *data++;
-        for (uint8_t j = 0; j < 8; j++) {
-            if (crc & 1) {
-                crc = (crc >> 1) ^ polynomial;
-            } else {
-                crc >>= 1;
-            }
-        }
+    crc32.reset();
+    for (uint16_t i = 0; i < length; i++) {
+        crc32.add(data[i]);
     }
-
-    return ~crc; // 取反得到最终的CRC32值
+    return crc32.value();
 }
