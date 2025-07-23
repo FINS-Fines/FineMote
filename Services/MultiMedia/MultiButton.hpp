6d921d5ccaa5636666473c56ddbab9f9866ad6fb
Liu Yucheng <liuyucheng720@gmail.com>
2025-07-23 15:49:54 +0800

    feat(Services): 把功能函数分离出来

--- Services/MultiMedia/MultiButton.hpp
+++ Services/MultiMedia/MultiButton.hpp
@@ -1,14 +1,11 @@
 
 #ifndef MULTI_BUTTON_HPP
-#define MULTI_BUTTON_HPP// ButtonDebounce.hpp
+#define MULTI_BUTTON_HPP
 #include <cstdint>
 #include <functional>
 #include <utility>
 #include "MultiMedia/BeepMusic.hpp"
 #include "MultiMedia/MultiButton.hpp"
-
-static int songIndex = 0;
-BeepMusic<BUZZER_PWM_ID> MusicBuzzer(0);
 
 constexpr uint32_t SHORT_TICKS = 200 ;
 constexpr uint32_t LONG_TICKS = 1000 ;
@@ -18,92 +15,90 @@ constexpr uint8_t PRESS_REPEAT_MAX_NUM = 2;
 class Button;
 using ButtonCallback = void (*)(Button*);
 // enum class ButtonEvent : uint8_t {
-//     BTN_PRESS_DOWN = 0,
-//     BTN_PRESS_UP,
-//     BTN_PRESS_REPEAT,
-//     BTN_SINGLE_CLICK,
-//     BTN_DOUBLE_CLICK,
-//     BTN_LONG_PRESS_START,
-//     BTN_LONG_PRESS_HOLD,
-//     BTN_EVENT_COUNT,
-//     BTN_NONE_PRESS
+//     PressDown = 0,
+//     PressUp,
+//     PressRepeat,
+//     SingleClick,
+//     DoubleClick,
+//     LongPressStart,
+//     LongPressHold,
+//     NonePress
 // };
 enum class ButtonState : uint8_t {
-    BTN_STATE_IDLE = 0,
-    BTN_STATE_PRESS,
-    BTN_STATE_RELEASE,
-    BTN_STATE_REPEAT,
-    BTN_STATE_LONG_HOLD
+    StateIdle = 0,
+    StatePress,
+    StateRelease,
+    StateRepeat,
+    StateLongHold
 };
 class Button : public DeviceBase{
 public:
-    template <typename conditionFunc>
-    Button (conditionFunc&& condition ) : isButtonPressed(std::forward<conditionFunc>(condition)) {
-        // SetDivisionFactor(5);
+    template <typename conditionFunc , typename exeFunc>
+    Button (conditionFunc&& condition , exeFunc&& execute) : IsButtonPressed(std::forward<conditionFunc>(condition)) ,ExecuteFunction(std::forward<exeFunc>(execute)){
+        SetDivisionFactor(5);
         ticks = 0;
         gapTicks = 0;
         // repeat = 0;
         isButtonBeingPressed = false;
-        state = ButtonState::BTN_STATE_IDLE;
+        state = ButtonState::StateIdle;
         // event = ButtonEvent::BTN_NONE_PRESS;
     }
     void Handle() final {
         ticks++;
-        if (isButtonPressed()) isButtonBeingPressed = true;
+        if (IsButtonPressed()) isButtonBeingPressed = true;
         else isButtonBeingPressed = false;
         switch (state) {
-            case ButtonState::BTN_STATE_IDLE:
-                Implement_Idle();
+            case ButtonState::StateIdle:
+                ImplementIdle();
                 break;
-            case ButtonState::BTN_STATE_PRESS:
-                Implement_Press();
+            case ButtonState::StatePress:
+                ImplementPress();
                 break;
-            case ButtonState::BTN_STATE_RELEASE:
-                Implement_Release();
+            case ButtonState::StateRelease:
+                ImplementRelease();
                 break;
-            case ButtonState::BTN_STATE_REPEAT:
-                Implement_Repeat();
+            case ButtonState::StateRepeat:
+                ImplementRepeat();
                 break;
-            case ButtonState::BTN_STATE_LONG_HOLD:
-                Implement_LongHold();
+            case ButtonState::StateLongHold:
+                ImplementLongHold();
                 break;
         }
     }
 
-    void Implement_Idle() {
+    void ImplementIdle() {
         if (isButtonBeingPressed) {
             if (ticks > SHORT_TICKS) {
-                state = ButtonState::BTN_STATE_PRESS;
+                state = ButtonState::StatePress;
             }
         }
         else {
             ticks = 0;
         }
     }
-    void Implement_Press() {
+    void ImplementPress() {
         if (!isButtonBeingPressed) {
-            state = ButtonState::BTN_STATE_RELEASE;
+            state = ButtonState::StateRelease;
             ticks = 0;
         }
         else {
             if (ticks > LONG_TICKS) {
-                state = ButtonState::BTN_STATE_LONG_HOLD;
+                state = ButtonState::StateLongHold;
             }
         }
     }
-    void Implement_Release() {
+    void ImplementRelease() {
         gapTicks++;
         if (gapTicks > GAP_TICKS) {
             ticks = 0;
             gapTicks = 0;
-            songIndex = (songIndex + 1) % 5;
-            MusicBuzzer.Play(songIndex);                        // 单击 判定完毕 播放 songIndex
+            ExecuteFunction(ButtonState::StateRelease);  // 单击 判定完毕 播放 songIndex
         }
         else {
             if (isButtonBeingPressed) {
                 if (ticks > SHORT_TICKS) {
                     gapTicks = 0;
-                    state = ButtonState::BTN_STATE_REPEAT;
+                    state = ButtonState::StateRepeat;
                 }
             }
             else {
@@ -111,23 +106,23 @@ public:
             }
         }
     }
-    void Implement_Repeat() {
+    void ImplementRepeat() {
         if (!isButtonBeingPressed) {
-            MusicBuzzer.Play(3);                       // 双击 判定完毕 播放index 3
+            ExecuteFunction(ButtonState::StateRepeat);    // 双击 判定完毕 播放index 3
             ticks = 0;
-            state = ButtonState::BTN_STATE_IDLE;
+            state = ButtonState::StateIdle;
+        }
+    }
+    void ImplementLongHold() {
+        if (!isButtonBeingPressed) {
+            ticks = 0;
+            state = ButtonState::StateRelease;
+            ExecuteFunction(ButtonState::StateLongHold);  // 长按 判定完毕 播放 index 4
         }
     }
 
-    void Implement_LongHold() {
-        if (!isButtonBeingPressed) {
-            ticks = 0;
-            state = ButtonState::BTN_STATE_RELEASE;
-            MusicBuzzer.Play(4);                         // 长按 判定完毕 播放 index 4
-        }
-    }
-
-    std::function<bool()> isButtonPressed;
+    std::function<bool()> IsButtonPressed;
+    std::function<void(ButtonState)> ExecuteFunction;
 
     int ticks ;
     int gapTicks;
