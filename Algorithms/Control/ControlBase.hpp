7174ca82c09c9341131636a9f0be121cd4861684
menghanyu <stevenmhy@sjtu.edu.cn>
2025-09-14 20:36:32 +0800

    feat(Algorithms): 删除重复的 ControllerBaseImpl
    (cherry picked from commit 918f5c75f6f8a5acc69853a949f73b468bf9bcc0)

--- Algorithms/Control/ControlBase.hpp
+++ Algorithms/Control/ControlBase.hpp
@@ -56,49 +56,4 @@ auto CreateControllers(Args&&... args) {
     return CreateControllersImpl<T<N>, M>(std::make_index_sequence<M>{}, std::forward<Args>(args)...);
 }
 
-template <size_t InputSize, size_t OutputSize>
-class ControllerBaseImpl  : public ControllerBase {
-public:
-    ControllerBaseImpl() = default;
-    virtual ~ControllerBaseImpl() = default;
-
-    template <size_t NextInputSize, size_t NextOutputSize>
-    void Cascade( ControllerBaseImpl<NextInputSize, NextOutputSize>& nextController ) {
-        static_assert(OutputSize == NextInputSize, "Output size of current controller must match input size of the next one.");
-        nextController.SetTargets(this->outputs);
-        nextCalc = std::bind(&ControllerBaseImpl<NextInputSize, NextOutputSize>::Calc, &nextController);
-    }
-
-    void SetTargets(std::array<float, InputSize>& sourceOutputs) {
-        for (size_t i = 0; i < InputSize; ++i) {
-            targetPtrs[i] = &sourceOutputs[i];
-        }
-    }
-
-    template <typename... Args>
-    void SetTargets(Args... args) {
-        static_assert(sizeof...(args) == InputSize, "Number of target pointers must match controller's input size.");
-        targetPtrs = {args...};
-    }
-
-
-    template <typename... Args>
-    void SetFeedbacks(Args... args) {
-        static_assert(sizeof...(args) == InputSize, "Number of feedback pointers must match controller's input size.");
-        feedbackPtrs = {args...};
-    }
-
-
-    ControllerOutputData GetOutputs() const {
-        return { outputs.data(), OutputSize };
-    }
-
-protected:
-
-    std::array<float*, InputSize> targetPtrs{};
-    std::array<float*, InputSize> feedbackPtrs{};
-    std::array<float, OutputSize> outputs{};
-};
-
-
 #endif
