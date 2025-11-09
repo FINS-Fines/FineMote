8916b9df5858c2c68a88c3207acc08a56226faad
Deng Mo（邓默） <tim-677@sjtu.edu.cn>
2025-11-09 16:32:26 +0800

    feat(BSP): FineMote新增对MC_Board02的支持

---
+++ BSP/MC_Board_02/MC_Board_02.cpp
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2024.
+ * IWIN-FINS Lab, Shanghai Jiao Tong University, Shanghai, China.
+ * All rights reserved.
+ ******************************************************************************/
+
+#include "MC_Board_02.h"
+
+//extern DMA_HandleTypeDef hdma_spi2_rx;
+//extern DMA_HandleTypeDef hdma_spi2_tx;
+
+//SPI_WITH_DMA_t spiWithDMA{&hspi2,&hdma_spi2_rx,&hdma_spi2_tx,&htim3,TIM_CHANNEL_2};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void BSP_Setup() {
+    HAL_TIM_Base_Start_IT(&TIM_Control);
+}
+
+#ifdef __cplusplus
+}
+#endif
