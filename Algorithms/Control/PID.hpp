ca48839313b6826c190f31a584b9efbac08a8dd9
Konieg <2622595404@qq.com>
2026-01-16 14:43:25 +0800

    fix(PID): 增加积分项和输出的限幅

--- Algorithms/Control/PID.hpp
+++ Algorithms/Control/PID.hpp
@@ -13,6 +13,13 @@ typedef struct PID_Param_t {
     float kp;
     float ki;
     float kd;
+
+    // 积分上下限
+    float iMax;
+
+    // 输出上下限
+    float outMax;
+
 } PID_Param_t;
 
 class PID : public ImplementControllerBase<1, 1> {
@@ -22,8 +29,11 @@ public:
     void PerformCalc() override {
         float error = *(this->targetPtrs[0]) - *(this->feedbackPtrs[0]);
         totalError += error;
-        //Clamp(totalError, -params.iMax, params.iMax);
-        this->outputs[0] = params.kp * error + params.ki * totalError + params.kd * (error - lastError);
+        Clamp(totalError, -params.iMax, params.iMax);       // 积分项限幅
+
+        float output = params.kp * error + params.ki * totalError + params.kd * (error - lastError);
+        this->outputs[0] = Clamp(output, -params.outMax, params.outMax);  // 输出限幅
+
         lastError = error;
     }
 
@@ -35,6 +45,45 @@ private:
     const PID_Param_t params;
     float totalError = 0;
     float lastError = 0;
+};
+
+template<size_t K>
+class CascadePID : public PID {
+public:
+    template<typename... Params>
+    constexpr explicit CascadePID(const PID_Param_t& first, Params... params) : PID(first), nodes{PID(params)...} {
+        static_assert(K > 1, "CascadePID should have at least 2 layers");
+
+        Cascade(nodes[0]);
+        for (int i = 0; i < K - 2; ++i) {
+            nodes[i].Cascade(nodes[i + 1]);
+        }
+    }
+
+    void PerformCalc() final{
+        PID::Calc();
+        for(auto& node : nodes) {
+            node.Calc();
+        }
+
+        this->outputs[0] = nodes.rbegin()->GetOutput();
+    }
+
+    void SetFeedback(const std::vector<float*>& feedbackPtrs) {
+        auto iter = feedbackPtrs.begin();
+
+        PID::SetFeedbacks(*iter);
+        ++iter;
+
+        for (auto& node : nodes) {
+            node.SetFeedbacks(*iter);
+            ++iter;
+        }
+    }
+
+
+private:
+    std::array<PID, K - 1> nodes;
 };
 
 #endif
