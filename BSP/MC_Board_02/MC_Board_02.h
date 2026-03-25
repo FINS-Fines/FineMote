1266c2aaef6542c1661ec993c19de755ab2919d3
Wanyin <3114751386@qq.com>
2026-03-26 01:46:32 +0800

    feat(build): add micro-ROS static library support and configure micro-ROS for MC_Board_02
    - add the FINEMOTE_MICROROS_VARIANT variable setting to the `board_toolchain.cmake` file
    - fix `BSP_UARTList` indexing
    - add `stm32h7+dap-link.cfg`

--- BSP/MC_Board_02/MC_Board_02.h
+++ BSP/MC_Board_02/MC_Board_02.h
@@ -21,27 +21,34 @@
 
 int main();
 
-class PeripheralsInit {
-
-    PeripheralsInit() {
+class PeripheralsInit
+{
+    PeripheralsInit()
+    {
         main();
     }
 
 public:
-    PeripheralsInit(const PeripheralsInit &) = delete;
+    PeripheralsInit(const PeripheralsInit&) = delete;
 
-    PeripheralsInit &operator=(const PeripheralsInit &) = delete;
+    PeripheralsInit& operator =(const PeripheralsInit&
+    )
+    = delete;
 
-    static PeripheralsInit &GetInstance() {
+    static PeripheralsInit
+    & GetInstance()
+    {
         static PeripheralsInit instance;
         return instance;
     }
 };
+
 /**
  * UART Definitions
  */
- // 新增了对SBUS端口(UART5)的支持
-constexpr UART_HandleTypeDef *BSP_UARTList[] = {nullptr, &huart1, &huart2, &huart5, &huart3, &huart7,&huart10};
+constexpr UART_HandleTypeDef* BSP_UARTList[] = {
+    nullptr, &huart1, &huart2, &huart3, nullptr, &huart5, nullptr, &huart7, nullptr, nullptr, &huart10
+};
 constexpr size_t UART_BUS_MAXIMUM_COUNT = sizeof(BSP_UARTList) / sizeof(BSP_UARTList[0]) - 1;
 
 /**
@@ -50,13 +57,13 @@ constexpr size_t UART_BUS_MAXIMUM_COUNT = sizeof(BSP_UARTList) / sizeof(BSP_UART
 constexpr size_t BSP_RS485UARTIndexList[] = {0, 4, 2};
 constexpr size_t RS485_BUS_MAXIMUM_COUNT = sizeof(BSP_RS485UARTIndexList) / sizeof(BSP_RS485UARTIndexList[0]) - 1;
 
-inline GPIO_TypeDef *const BSP_RS485FlowCtrlPortList[3] = {nullptr, GPIOB, GPIOD};
+inline GPIO_TypeDef* const BSP_RS485FlowCtrlPortList[3] = {nullptr, GPIOB, GPIOD};
 constexpr uint16_t BSP_RS485FlowCtrlPinList[3] = {0, GPIO_PIN_14, GPIO_PIN_4};
 
 /**
  * CAN Definitions
  */
-constexpr FDCAN_HandleTypeDef *BSP_CANList[] = {nullptr, &hfdcan1, &hfdcan2, &hfdcan3};
+constexpr FDCAN_HandleTypeDef* BSP_CANList[] = {nullptr, &hfdcan1, &hfdcan2, &hfdcan3};
 constexpr size_t CAN_BUS_MAXIMUM_COUNT = sizeof(BSP_CANList) / sizeof(BSP_CANList[0]) - 1;
 
 /**
@@ -67,19 +74,20 @@ constexpr size_t CAN_BUS_MAXIMUM_COUNT = sizeof(BSP_CANList) / sizeof(BSP_CANLis
 /**
  * PWM Definitions
  */
-using PWMList_t = struct PWMList_t {
-  uint32_t TIM_CHANNEL;
-  TIM_HandleTypeDef *TIM_Handle = nullptr;
-  uint16_t TIM_Frequency = 168; // Default frequency
+using PWMList_t = struct PWMList_t
+{
+    uint32_t TIM_CHANNEL;
+    TIM_HandleTypeDef* TIM_Handle = nullptr;
+    uint16_t TIM_Frequency = 168; // Default frequency
 };
 
 constexpr PWMList_t BSP_PWMList[6] = {
-  {0, nullptr, 0},
-  {TIM_CHANNEL_1, &htim2, 240},
-  {TIM_CHANNEL_3, &htim2, 240},
-  {TIM_CHANNEL_1, &htim1, 240},
-  {TIM_CHANNEL_3, &htim1, 240},
-  {TIM_CHANNEL_2, &htim12, 240}  // BUZZER_PWM
+    {0, nullptr, 0},
+    {TIM_CHANNEL_1, &htim2, 240},
+    {TIM_CHANNEL_3, &htim2, 240},
+    {TIM_CHANNEL_1, &htim1, 240},
+    {TIM_CHANNEL_3, &htim1, 240},
+    {TIM_CHANNEL_2, &htim12, 240} // BUZZER_PWM
 };
 
 /**
@@ -96,11 +104,13 @@ constexpr size_t BUZZER_PWM_ID = 5;
 
 #define TIM_Control htim7
 #define hiwdg hiwdg1
-typedef struct {
-    SPI_HandleTypeDef *spiHandle;
-    DMA_HandleTypeDef *rxDMAHandle;
-    DMA_HandleTypeDef *txDMAHandle;
-    TIM_HandleTypeDef *timHandleForHeat;
+
+typedef struct
+{
+    SPI_HandleTypeDef* spiHandle;
+    DMA_HandleTypeDef* rxDMAHandle;
+    DMA_HandleTypeDef* txDMAHandle;
+    TIM_HandleTypeDef* timHandleForHeat;
     uint32_t timChannelForHeat;
 } SPI_WITH_DMA_t;
 
@@ -110,6 +120,6 @@ extern SPI_WITH_DMA_t spiWithDMA;
  * MICRO_ROS Definitions
  */
 static constexpr bool WITH_MICRO_ROS = true;
-static constexpr size_t MICRO_ROS_UART_ID = 3;
+static constexpr size_t MICRO_ROS_UART_ID = 1;
 
 #endif
--- BSP/Robomaster_C/Robomaster_C.h
+++ BSP/Robomaster_C/Robomaster_C.h
@@ -20,18 +20,22 @@
 
 int main();
 
-class PeripheralsInit{
-/**
- *  @attention 此函数需要与cube生成的保持一致
- */
-    PeripheralsInit() {
+class PeripheralsInit
+{
+    /**
+     *  @attention 此函数需要与cube生成的保持一致
+     */
+    PeripheralsInit()
+    {
         main();
     }
 
 public:
-    PeripheralsInit(const PeripheralsInit &) = delete;
+    PeripheralsInit(const PeripheralsInit&) = delete;
     PeripheralsInit& operator=(const PeripheralsInit&) = delete;
-    static PeripheralsInit& GetInstance(){
+
+    static PeripheralsInit& GetInstance()
+    {
         static PeripheralsInit instance;
         return instance;
     }
@@ -40,13 +44,13 @@ public:
 /**
  * UART Definitions
  */
-constexpr UART_HandleTypeDef *BSP_UARTList[] = {nullptr, &huart6, &huart1, &huart3};
+constexpr UART_HandleTypeDef* BSP_UARTList[] = {nullptr, &huart1, nullptr, &huart3, nullptr, nullptr, &huart6,};
 constexpr size_t UART_BUS_MAXIMUM_COUNT = sizeof(BSP_UARTList) / sizeof(BSP_UARTList[0]) - 1;
 
 /**
  * CAN Definitions
  */
-constexpr CAN_HandleTypeDef *BSP_CANList[] = {nullptr, &hcan1, &hcan2};
+constexpr CAN_HandleTypeDef* BSP_CANList[] = {nullptr, &hcan1, &hcan2};
 constexpr size_t CAN_BUS_MAXIMUM_COUNT = sizeof(BSP_CANList) / sizeof(BSP_CANList[0]) - 1;
 
 /**
@@ -56,22 +60,23 @@ constexpr size_t CAN_BUS_MAXIMUM_COUNT = sizeof(BSP_CANList) / sizeof(BSP_CANLis
 /**
  * PWM Definitions
  */
-using PWMList_t = struct PWMList_t {
-  uint32_t TIM_CHANNEL;
-  TIM_HandleTypeDef *TIM_Handle = nullptr;
-  uint16_t TIM_Frequency = 168; // Default frequency
+using PWMList_t = struct PWMList_t
+{
+    uint32_t TIM_CHANNEL;
+    TIM_HandleTypeDef* TIM_Handle = nullptr;
+    uint16_t TIM_Frequency = 168; // Default frequency
 };
 
 constexpr PWMList_t BSP_PWMList[9] = {
-  {0, nullptr, 0},
-  {TIM_CHANNEL_1, &htim1, 168},
-  {TIM_CHANNEL_2, &htim1, 168},
-  {TIM_CHANNEL_3, &htim1, 168},
-  {TIM_CHANNEL_4, &htim1, 168},
-  {TIM_CHANNEL_1, &htim8, 168},
-  {TIM_CHANNEL_2, &htim8, 168},
-  {TIM_CHANNEL_3, &htim8, 168},
-  {TIM_CHANNEL_3, &htim4, 84}  // BUZZER_PWM
+    {0, nullptr, 0},
+    {TIM_CHANNEL_1, &htim1, 168},
+    {TIM_CHANNEL_2, &htim1, 168},
+    {TIM_CHANNEL_3, &htim1, 168},
+    {TIM_CHANNEL_4, &htim1, 168},
+    {TIM_CHANNEL_1, &htim8, 168},
+    {TIM_CHANNEL_2, &htim8, 168},
+    {TIM_CHANNEL_3, &htim8, 168},
+    {TIM_CHANNEL_3, &htim4, 84} // BUZZER_PWM
 };
 
 /**
@@ -88,13 +93,21 @@ constexpr size_t BUZZER_PWM_ID = 8;
 
 #define USER_I2C hi2c2
 
-typedef struct {
+typedef struct
+{
     SPI_HandleTypeDef* spiHandle;
     DMA_HandleTypeDef* rxDMAHandle;
     DMA_HandleTypeDef* txDMAHandle;
     TIM_HandleTypeDef* timHandleForHeat;
     uint32_t timChannelForHeat;
-}SPI_WITH_DMA_t;
+} SPI_WITH_DMA_t;
+
 extern SPI_WITH_DMA_t spiWithDMA;
+
+/**
+ * MICRO_ROS Definitions
+ */
+static constexpr bool WITH_MICRO_ROS = true;
+static constexpr size_t MICRO_ROS_UART_ID = 6;
 
 #endif
