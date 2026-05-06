eaa936ddd773ba41d9251519b8b448f6912ceb64
Feiran Wei <wfrfred@gmail.com>
2026-05-06 18:00:07 +0800

    feat(Devices): use posix instead of cmsis interface

--- Devices/DeviceBase/DeviceScheduler.cpp
+++ Devices/DeviceBase/DeviceScheduler.cpp
@@ -3,18 +3,32 @@
 //
 #include "DeviceScheduler.hpp"
 #include "DeviceBase.hpp"
-#include "cmsis_os2.h"
 
 #include <etl/algorithm.h>
+#include <FreeRTOS_POSIX/time.h>
 
-void DeviceScheduler::RegisterDevice(DeviceBase* device) {
+namespace {
+    void AddTicksToTimespec(timespec &time, const uint32_t ticks) {
+        const int64_t nanoseconds = static_cast<int64_t>(ticks) * NANOSECONDS_PER_TICK;
+
+        time.tv_sec += nanoseconds / NANOSECONDS_PER_SECOND;
+        time.tv_nsec += static_cast<long>(nanoseconds % NANOSECONDS_PER_SECOND);
+
+        if (time.tv_nsec >= NANOSECONDS_PER_SECOND) {
+            time.tv_sec += time.tv_nsec / NANOSECONDS_PER_SECOND;
+            time.tv_nsec %= NANOSECONDS_PER_SECOND;
+        }
+    }
+} // namespace
+
+void DeviceScheduler::RegisterDevice(DeviceBase *device) {
     if (running_) {
         return;
     }
 
     const auto period = device->divisionFactor;
 
-    const auto it = etl::find_if(buckets_.begin(), buckets_.end(), [period](const Bucket& b) {
+    const auto it = etl::find_if(buckets_.begin(), buckets_.end(), [period](const Bucket &b) {
         return b.period == period;
     });
 
@@ -41,14 +55,15 @@ void DeviceScheduler::Start() {
         return;
     }
 
-    etl::sort(buckets_.begin(), buckets_.end(), [](const Bucket& a, const Bucket& b) {
+    etl::sort(buckets_.begin(), buckets_.end(), [](const Bucket &a, const Bucket &b) {
         return a.period < b.period;
     });
 
     int priority = MAX_SCHEDULER_PRIORITY;
 
-    for (Bucket& bucket: buckets_) {
-        sched_param param { .sched_priority = priority };
+    for (Bucket &bucket: buckets_) {
+        sched_param param{};
+        param.sched_priority = priority;
 
         pthread_attr_t attr;
         pthread_attr_init(&attr);
@@ -66,14 +81,17 @@ void DeviceScheduler::Start() {
     running_ = true;
 }
 
-[[noreturn]] void* DeviceScheduler::BucketThreadFunc(void* arg) {
-    auto* bucket = static_cast<Bucket*>(arg);
-    auto& devices = bucket->devices;
+[[noreturn]] void *DeviceScheduler::BucketThreadFunc(void *arg) {
+    auto *bucket = static_cast<Bucket *>(arg);
+    auto &devices = bucket->devices;
 
     const uint32_t period = bucket->period;
+    timespec next_wake_time{};
+    clock_gettime(CLOCK_MONOTONIC, &next_wake_time);
 
     while (true) {
-        const uint32_t last_wake_time = osKernelGetTickCount();
+        AddTicksToTimespec(next_wake_time, period);
+
         for (const auto device: devices) {
             device->Update();
             device->updated = true;
@@ -85,6 +103,7 @@ void DeviceScheduler::Start() {
                 device->updated = false;
             }
         }
-        osDelayUntil(last_wake_time + period);
+
+        clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &next_wake_time, nullptr);
     }
 }
