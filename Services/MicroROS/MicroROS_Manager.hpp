2c8ef60eb31c62dedad3f5854e3b2a11c59d77a3
Wanyin <3114751386@qq.com>
2026-05-12 03:20:11 +0800

    feat(Services): add micro-ROS service framework
    Add micro-ROS agent, manager, message type definitions and transport
    abstraction layer for ROS 2 communication on embedded targets.

---
+++ Services/MicroROS/MicroROS_Manager.hpp
@@ -0,0 +1,277 @@
+/*******************************************************************************
+ * Copyright (c) 2026.
+ * IWIN-FINS Lab, Shanghai Jiao Tong University, Shanghai, China.
+ * All rights reserved.
+ ******************************************************************************/
+
+#ifndef FINEMOTE_MICROROS_MANAGER_HPP
+#define FINEMOTE_MICROROS_MANAGER_HPP
+
+#include "Board.h"
+#include "FreeRTOS.h"
+#include "cmsis_os.h"
+#include "etl/list.h"
+#include "stream_buffer.h"
+#include "task.h"
+
+#include <FreeRTOS_POSIX.h>
+#include <FreeRTOS_POSIX/pthread.h>
+
+#include <rcl/rcl.h>
+#include <rclc/executor.h>
+#include <rclc/rclc.h>
+#include <rmw_microros/rmw_microros.h>
+#include <uxr/client/transport.h>
+
+#include "Bus/UART_Base.hpp"
+#include "MicroROS_Agent.hpp"
+#include "MicroROS_Transport.hpp"
+
+constexpr size_t MICROROS_MAX_HANDLES = 10;
+constexpr size_t MICROROS_MAX_AGENTS = 10;
+
+template <bool enable>
+class MicroROS_Manager
+{
+    friend class MicroROS_Transport<enable>;
+
+public:
+    enum class State { WAITING_AGENT, INITIALIZING, RUNNING, ERROR };
+
+    static MicroROS_Manager& GetInstance()
+    {
+        static MicroROS_Manager instance;
+        return instance;
+    }
+
+    void RegisterAgent(ROSAgent<>* agent)
+    {
+        if (!agents_.full())
+        {
+            agents_.push_back(agent);
+        }
+    }
+
+    void Handle()
+    {
+        switch (state_)
+        {
+        case State::WAITING_AGENT:
+            HandleWaiting();
+            break;
+        case State::INITIALIZING:
+            HandleInitializing();
+            break;
+        case State::RUNNING:
+            HandleRunning();
+            break;
+        case State::ERROR:
+            osDelay(1000);
+            state_ = State::WAITING_AGENT;
+            break;
+        }
+    }
+
+private:
+    MicroROS_Manager() :
+        dma_buffer_([this](uint8_t* data, size_t size)
+        {
+            this->PushRxData(data, size);
+        })
+    {
+        Setup();
+        StartThread();
+    }
+
+    void Setup()
+    {
+        allocator_ = rcl_get_default_allocator();
+
+        rx_stream_buffer_ = xStreamBufferCreateStatic(
+            MICROROS_BUF_SIZE,
+            1,
+            stream_buffer_storage_,
+            &stream_buffer_struct_
+        );
+
+        tx_sem_ = xSemaphoreCreateBinaryStatic(&tx_sem_struct_);
+        configASSERT(tx_sem_ != nullptr);
+        xSemaphoreGive(tx_sem_);
+
+        UART_Base<MICRO_ROS_UART_ID>::GetInstance().BindTxHandle([this]()
+        {
+            BaseType_t hpw = pdFALSE;
+            xSemaphoreGiveFromISR(this->tx_sem_, &hpw);
+            return true;
+        });
+
+        rmw_uros_set_custom_transport(true, nullptr, MicroROS_Transport<enable>::Open,
+                                      MicroROS_Transport<enable>::Close, MicroROS_Transport<enable>::Write,
+                                      MicroROS_Transport<enable>::Read);
+    }
+
+    void StartThread()
+    {
+        pthread_attr_t attr;
+        pthread_attr_init(&attr);
+        pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+
+        constexpr size_t STACK_SIZE = 20 * 1024;
+        pthread_attr_setstacksize(&attr, STACK_SIZE);
+
+        if (pthread_create(&thread_, &attr, &MicroROS_Manager::ThreadFunc, this) != 0)
+        {
+            HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+        }
+        pthread_attr_destroy(&attr);
+    }
+
+    [[noreturn]] static void* ThreadFunc(void* arg)
+    {
+        auto* manager = static_cast<MicroROS_Manager*>(arg);
+
+        for (;;)
+        {
+            manager->Handle();
+            osDelay(200);
+        }
+    }
+
+    ~MicroROS_Manager() = default;
+
+    void PushRxData(uint8_t* data, size_t size)
+    {
+        xStreamBufferSendFromISR(
+            rx_stream_buffer_,
+            data,
+            size,
+            nullptr
+        );
+    }
+
+    void HandleWaiting()
+    {
+        if (rmw_uros_ping_agent(500, 1) == RMW_RET_OK)
+        {
+            state_ = State::INITIALIZING;
+        }
+    }
+
+    void HandleInitializing()
+    {
+        rcl_ret_t ret;
+
+        ret = rclc_support_init(&support_, 0, nullptr, &allocator_);
+        if (ret != RCL_RET_OK)
+        {
+            GotoError();
+            return;
+        }
+
+        ret = rclc_node_init_default(&node_, MICROROS_NODE_NAME, "", &support_);
+        if (ret != RCL_RET_OK)
+        {
+            GotoError();
+            return;
+        }
+
+        ret = rclc_executor_init(&executor_, &support_.context, MICROROS_MAX_HANDLES, &allocator_);
+        if (ret != RCL_RET_OK)
+        {
+            GotoError();
+            return;
+        }
+
+        for (auto* agent : agents_)
+        {
+            if (!agent->Init(&node_, &support_, &executor_))
+            {
+                GotoError();
+                return;
+            }
+        }
+
+        last_tick_ = xTaskGetTickCount();
+        state_ = State::RUNNING;
+    }
+
+    void HandleRunning()
+    {
+        rcl_ret_t ret = rclc_executor_spin_some(&executor_, RCL_MS_TO_NS(10));
+
+        if (ret != RCL_RET_OK && ret != RCL_RET_TIMEOUT)
+        {
+            GotoError();
+            return;
+        }
+
+        for (auto* agent : agents_)
+        {
+            agent->Execute();
+        }
+
+        if ((xTaskGetTickCount() - last_tick_) > pdMS_TO_TICKS(1000))
+        {
+            last_tick_ = xTaskGetTickCount();
+            if (rmw_uros_ping_agent(500, 1) != RMW_RET_OK)
+            {
+                GotoError();
+            }
+        }
+    }
+
+    void GotoError()
+    {
+        Cleanup();
+        state_ = State::ERROR;
+    }
+
+    void Cleanup()
+    {
+        for (auto* agent : agents_)
+        {
+            agent->Fini();
+        }
+        (void)rclc_executor_fini(&executor_);
+        (void)rcl_node_fini(&node_);
+        (void)rclc_support_fini(&support_);
+    }
+
+    State state_ = State::WAITING_AGENT;
+    rcl_allocator_t allocator_;
+    rclc_support_t support_;
+    rcl_node_t node_;
+    rclc_executor_t executor_;
+    uint32_t last_tick_ = 0;
+
+    StaticSemaphore_t tx_sem_struct_;
+    SemaphoreHandle_t tx_sem_;
+
+    StaticStreamBuffer_t stream_buffer_struct_;
+    uint8_t stream_buffer_storage_[MICROROS_BUF_SIZE + 1];
+    StreamBufferHandle_t rx_stream_buffer_;
+
+    etl::list<ROSAgent<>*, MICROROS_MAX_AGENTS> agents_;
+
+    uint8_t tx_buffer_[MICROROS_BUF_SIZE];
+    UARTBuffer<MICRO_ROS_UART_ID, MICROROS_DMA_BUF_SIZE> dma_buffer_;
+
+    pthread_t thread_{};
+};
+
+template <>
+class MicroROS_Manager<false>
+{
+public:
+    static MicroROS_Manager& GetInstance()
+    {
+        static_assert(
+            WITH_MICRO_ROS,
+            "MicroROS is disabled in Board.h. Please set WITH_MICRO_ROS = true to use MicroROS_Base."
+        );
+        static MicroROS_Manager instance;
+        return instance;
+    }
+};
+
+#endif
