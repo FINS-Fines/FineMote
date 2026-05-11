f4d6aa02b32afc5d633c1cee31c4a8f3165d7547
Wanyin <3114751386@qq.com>
2026-05-12 01:39:30 +0800

    fix(BSP): add Bus/ prefix to CAN_Base.hpp and CAN_Header.hpp includes

--- BSP/Robomaster_C/FineMote_BSP/BSP_CAN.h
+++ BSP/Robomaster_C/FineMote_BSP/BSP_CAN.h
@@ -8,8 +8,8 @@
 #define BSP_CAN_H
 
 #include "Board.h"
-#include "CAN_Base.hpp"
-#include "CAN_Header.hpp"
+#include "Bus/CAN_Base.hpp"
+#include "Bus/CAN_Header.hpp"
 class BSP_CANs {
 public:
     static BSP_CANs &GetInstance() {
