015b4bf3d16b2554cf46e1615b0013478e671b5d
Libingfu-Software <1564922632@qq.com>
2026-07-29 13:25:30 +0800

    refactor(Algorithms/Matrix):将矩阵运算改为通用版本，不依赖arm_math.h

--- Algorithms/Matrix/matrix.cpp
+++ Algorithms/Matrix/matrix.cpp
@@ -1,18 +1,18 @@
 /*******************************************************************************
-* Copyright (c) 2024.
+ * Copyright (c) 2026.
  * IWIN-FINS Lab, Shanghai Jiao Tong University, Shanghai, China.
  * All rights reserved.
  ******************************************************************************/
 
 #include "matrix.h"
 
-// hat of vector
 Matrixf<3, 3> vector3f::hat(Matrixf<3, 1> vec) {
-    float hat[9] = { 0, -vec[2][0], vec[1][0], vec[2][0], 0, -vec[0][0], -vec[1][0], vec[0][0], 0 };
-    return Matrixf<3, 3>(hat);
+  float hat[9] = {0.0f,       -vec[2][0], vec[1][0], vec[2][0], 0.0f,
+                  -vec[0][0], -vec[1][0], vec[0][0], 0.0f};
+  return Matrixf<3, 3>(hat);
 }
 
-// cross product
-Matrixf<3, 1> vector3f::cross(Matrixf<3, 1> vec1, Matrixf<3, 1> vec2) {
-    return vector3f::hat(vec1) * vec2;
+Matrixf<3, 1> vector3f::cross(Matrixf<3, 1> vec1,
+                              Matrixf<3, 1> vec2) {
+  return vector3f::hat(vec1) * vec2;
 }
