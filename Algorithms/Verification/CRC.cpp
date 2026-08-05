b693598011c8b4014639b92a4c68d36979df60c8
freude9 <75164063+freude9@users.noreply.github.com>
2026-08-05 18:41:46 +0800

    feat(Algorithms): 更新CRC的实现方式

--- Algorithms/Verification/CRC.cpp
+++ Algorithms/Verification/CRC.cpp
@@ -4,33 +4,4 @@
  * All rights reserved.
  ******************************************************************************/
 
-#include "CRC.h"
-
-uint8_t CRC8Calc(uint8_t* data, uint16_t length) {
-    static etl::crc8_ccitt crc8;
-
-    crc8.reset();
-    for (uint16_t i = 0; i < length; i++) {
-        crc8.add(data[i]);
-    }
-    return crc8.value();
-}
-
-uint16_t CRC16Calc(uint8_t* data, uint16_t length) {
-    etl::crc16_modbus crc16;
-
-    for (uint16_t i = 0; i < length; i++) {
-        crc16.add(data[i]);
-    }
-    return crc16.value();
-}
-
-uint32_t CRC32Calc(uint8_t* data, uint16_t length) {
-    static etl::crc32 crc32;
-
-    crc32.reset();
-    for (uint16_t i = 0; i < length; i++) {
-        crc32.add(data[i]);
-    }
-    return crc32.value();
-}
+#include "CRC.hpp"
